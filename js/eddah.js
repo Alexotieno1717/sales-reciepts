888cd758bb5be03773e91104604930134ff93eb5
eddah <eddahbaty98@gmail.com>
2020-08-06 14:16:10 +0300

    add checkout button , js function and styled using css

--- js/eddah.js
+++ js/eddah.js
@@ -1,3 +1,23 @@
-//onclick="document.getElementById('myInput').value = ''"
+$(document).ready(function(){
+    $("#form").hide()
+    $("#checkout").click(function () {
+        $("#form").show()
+    });
+    //validate delivery form
+    $("#confirm").click(function () {
+        event.preventDefault()
+        var firstname = $("#fname").val();
+        var email = $("#email").val();
+        var address = $("#adr").val();
 
-//<input type="text" value="Blabla" id="myInput"></input>
+        if (firstname == '' || email == '' || address == '' ) {
+            alert("Please fill all fields");
+        } else {
+            alert('Hello ' + name + " We have received your Credentials .The payments will be processed. Thankyou ");
+
+        }
+
+    });
+
+});
+
